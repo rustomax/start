6c17331489611d8d5870716b46e4e59ecca5a97d
Daniel <unbin1234@gmail.com>
2017-10-03 20:37:46 -0400

    fixed spacing

--- function.c
+++ function.c
@@ -8,15 +8,10 @@ int sum_of_x = x + sec;
 printf("sum of the two numbers = %d\n", sum_of_x);
 return sum_of_x;
 };
-
-
-
-
 int main(){
 printf("input a value\n");
 int fir = 0;
 scanf("%d",&fir);
 add (fir);
-
-    return 0;
+return 0;
 }
