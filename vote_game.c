3553f1789c1d9a21191e289bf4aa50822180d052
Daniel <unbin1234@gmail.com>
2017-10-03 20:49:32 -0400

    fixed some issues

--- vote_game.c
+++ vote_game.c
@@ -1,25 +1,11 @@
 #include <stdio.h>
-int main ()
+int main (void)
 {
-
     int age;
-
     printf ("input your age please.\n");
-
-
-scanf("%d",&age);
-
-
-if (age >= 18) {
-
-    printf("you can vote\n");
-
-
-} else {
-    printf("you can not vote\n");
-
+    scanf("%d",&age);
+    if (age >= 18) {
+        printf("you can vote\n");
 }
-
-return 0;
-
+    else printf("you can not vote\n");
 }
