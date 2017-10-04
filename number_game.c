1168aa784058c6b9cdbaaec8cfc2daf544d96929
Daniel <unbin1234@gmail.com>
2017-10-03 20:41:05 -0400

    fixed some issues

--- number_game.c
+++ number_game.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-int main ()
-{
-    int num;
-    printf("input anything higher than 5\n");
-    scanf("%d",&num);
-
-    if ( num > 5) {
-        printf("you win!\n");
-    } else {
-        printf("you loose ):\n");
-    }
-    return 0;
+int main (void){
+int num;
+printf("input anything higher than 5\n");
+scanf("%d",&num);
+if ( num > 5) {
+   printf("you win!\n");
+} 
+ else printf("you loose ):\n");
+ return 0;
 }
