6e927037626545c6755b1a18950d5451e9dfd0d1
Daniel <unbin1234@gmail.com>
2017-10-03 20:50:54 -0400

    mainly fixed spacing

--- writing_functions.c
+++ writing_functions.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 
-    int square_me (int x)
-    {
-        int square_of_x = x * x;
-
-        printf("%d\n",square_of_x );
-
-        return square_of_x;
-    }
-
-int main()
-{
+int square_me (int x){
+    int square_of_x = x * x;
+    printf("%d\n",square_of_x );
+    return square_of_x;
+}
+int main(void){
     int num;
-
     printf("input a value to find it's square\n");
-
     scanf("%d",&num );
-
     square_me(num);
-
-    return 0;
 }
