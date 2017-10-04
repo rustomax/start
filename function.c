21e7c6978c9eed94d9cb84b0400e1d0546b6ac4c
Daniel <unbin1234@gmail.com>
2017-10-03 20:45:31 -0400

    fixed spacing

--- function.c
+++ function.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 int add (int x){
-int sec;
-printf("input another value\n");
-scanf("%d",&sec);
-int sum_of_x = x + sec;
-printf("sum of the two numbers = %d\n", sum_of_x);
-return sum_of_x;
+  int sec;
+  printf("input another value\n");
+  scanf("%d",&sec);
+  int sum_of_x = x + sec;
+  printf("sum of the two numbers = %d\n", sum_of_x);
+  return sum_of_x;
 };
-int main(){
-printf("input a value\n");
-int fir = 0;
-scanf("%d",&fir);
-add (fir);
-return 0;
+int main(void){
+  printf("input a value\n");
+  int fir = 0;
+  scanf("%d",&fir);
+  add (fir);
 }
