2c2033b9532c04dcb8af9796f300d61d84498f5a
unbin <unbin1234@gmail.com>
2018-01-25 23:14:20 -0500

    Added features
    - Added the abiliy to change the board (Broken because of function changes).
    - Added a new object (Broken because of function changes).
    - Moved movement commands into a seperate function.

--- quest.c
+++ quest.c
@@ -2,8 +2,9 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-void board_print(int board[10][10]){
+void board_print(int board[10][10], int room, bool print_true){
   system("clear");
+  printf("Room = %d\n",room);
   printf("----------------------\n");
   for (int x = 0; x < 10; x++) {
     printf("|");
@@ -14,47 +15,134 @@ void board_print(int board[10][10]){
       if (board[x][y] == 1) {
         printf("O ");
       }
+      if (board[x][y] == 2) {
+        printf("# ");
+      }
     }
     printf("|\n");
   }
   printf("----------------------\n");
+  if (print_true == true) {
+    printf("This Is A Secret.\nTell Nobody!\n");
+  }
+}
+
+int action (int board[10][10], int *room, int *alt_x,int *alt_y, char *direction, bool *print_true){
+  char usr = 'a';
+  bool found = false;
+  scanf("%s",&usr);
+  for (int x = 0; x < 10 && !found; x++) {
+    for (int y = 0; y < 10 && !found; y++) {
+        if (board[x][y] == 1) {
+          found = true;
+          if (usr == 'w'){
+            //MOVE:
+            if (board[x - 1][y] == 0 && x != 0){
+              board[x - 1][y] = 1;
+              *alt_x = x - 1;
+              *alt_y = y;
+            }
+            //CHANGE ROOM:
+            if (x == 0){
+              if (room == 0){
+                *room = 1;
+              }
+              board[x][y] = 0;
+              board[9][y] = 1;
+            }
+          }
+          else if (usr == 'a'){
+            //MOVE:
+            if (board[x][y - 1] == 0 && y != 0){
+              board[x][y - 1] = 1;
+              *alt_x = x;
+              *alt_y = y - 1;
+            }
+            //CHANGE ROOM:
+            if (y == 0){
+              if (room == 0){
+                *room = 4;
+              }
+              board[x][y] = 0;
+              board[x][9] = 1;
+            }
+          }
+          else if (usr == 's'){
+            //MOVE:
+            if (board[x + 1][y] == 0 && x != 9){
+              board[x + 1][y] = 1;
+              *alt_x = x + 1;
+              *alt_y = y;
+            }
+            //CHANGE ROOM:
+            if (x == 9){
+              if (room == 0){
+                *room = 3;
+              }
+              board[x][y] = 0;
+              board[0][y] = 1;
+            }
+          }
+          else if (usr == 'd'){
+            //MOVE:
+            if (board[x][y + 1] == 0 && y != 9){
+              board[x][y + 1] = 1;
+              *alt_x = x;
+              *alt_y = y + 1;
+            }
+            //CHANGE ROOM:
+            if (y == 9){
+              if (room == 0){
+                *room = 2;
+              }
+              board[x][y] = 0;
+              board[x][0] = 1;
+            }
+          }
+          if (board[x - 1][y] == 1 || board[x + 1][y] == 1 || board[x][y - 1] == 1 || board[x][y + 1] == 1) {
+            board[x][y] = 0;
+          }
+          if ((board[x - 1][y] == 2 && usr == 'w') || (board[x + 1][y] == 2 && usr == 's') || (board[x][y - 1] == 2 && usr == 'a') || (board[x][y + 1] == 2 && usr == 'd')) {
+            *print_true = true;
+          }
+          *direction = usr;
+        }
+      }
+    }
+  return 0;
 }
 
 int main(void) {
   bool game = true;
   int room = 0;
   int board[10][10];
-  char usr;
+  int x = 0;
+  int y = 0;
+  char direction;
+  bool print_true = false;
   for (int x = 0; x < 10; x++) {
     for (int y = 0; y < 10; y++) {
       board[x][y] = 0;
     }
   }
   board[5][5] = 1;
-  board_print(board);
+  board[0][5] = 2;
+  board_print(board, room, print_true);
   while (game == true) {
-    bool found = false;
-    scanf("%s",&usr);
-    for (int x = 0; x < 10 && !found; x++) {
-      for (int y = 0; y < 10 && !found; y++) {
-          if (board[x][y] == 1) {
-            found = true;
-          if (usr == 'w'){
-              board[x - 1][y] = 1;
-          }
-          else if (usr == 'a'){
-              board[x][y - 1] = 1;
-          }
-            else if (usr == 's'){
-                board[x + 1][y] = 1;
-            }
-          else if (usr == 'd'){
-              board[x][y + 1] = 1;
-          }
-          board[x][y] = 0;
-        }
-      }
+    action (board, &room, &x, &y, &direction, &print_true);
+    board[x][y] = 1;
+    if (direction == 'w' && board[x + 1][y] == 1){
+      board[x + 1][y] = 0;
     }
-    board_print(board);
+    if (direction == 'a' && board[x][y + 1] == 1) {
+      board[x][y + 1] = 0;
+    }
+    if (direction == 's' && board[x - 1][y] == 1) {
+      board[x - 1][y] = 0;
+    }
+    if (direction == 'd' && board[x][y - 1] == 1) {
+      board[x][y - 1] = 0;
+    }
+    board_print(board, room, print_true);
   }
 }
